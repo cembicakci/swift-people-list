7a390f932c06eccdaa7205bb61334b23ec324ab1
Cem Bıçakcı <cmbicakci@gmail.com>
2023-11-04 11:54:44 +0300

    added json data

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Cem Bıçakcı on 4.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
